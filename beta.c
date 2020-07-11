d0eeddfd6859cc82b595fab58552cb7946dc5988
yuta <tmon.seveny@gmail.com>
2020-07-11 19:55:20 +0900

    変更予定のサンプリングアルゴリズムを改良

--- beta.c
+++ beta.c
@@ -1,223 +1,165 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
 
-int UNIFORM_RANGE = 5;
-
-struct Uniform
+struct Sample
 {
-    float value;
-    struct Uniform *prev;
-    struct Uniform *next;
+    int result;
+    struct Sample *next;
 };
 
 struct Position
 {
-    int a;
-    int b;
-    struct Uniform *sample;
+    //uint64_t m;
+    //uint64_t y;
+    struct Sample *sample;
+    int max_sample_length;
+    int sample_length;
 };
 
-struct Uniform* CreateUniform(float uniform, struct Uniform *prev, struct Uniform *next)
+struct Sample* CreateSample(int result, struct Sample *next)
 {
-    struct Uniform *node = (struct Uniform *)malloc(sizeof(struct Uniform));
-    node->value = uniform;
-    node->prev = prev;
-    node->next = next;
+    struct Sample *sample = (struct Sample *)malloc(sizeof(struct Sample));
+    sample->result = result;
+    sample->next = next;
+    return sample;
+};
 
-    return node;
-}
-
-// Uniformを追加する処理
-// RANGE外の処理は別
-void AddUniform(struct Uniform *node, float uniform, char direction)
-{
-    struct Uniform *target = node;
-
-    switch (direction)
-    {
-        case 'l':
-            // 終了条件
-            if (target == NULL)
-            {
-                target = CreateUniform(uniform, NULL, target->next);
-            }
-            else if (target->value < uniform)
-            {
-                struct Uniform *new = CreateUniform(uniform, target, target->next);
-                target->next = new;
-                target->next->prev = new;
-            }
-            else
-            {
-                AddUniform(target->prev, uniform, 'l');
-            }
-            break;
-        
-        case 'r':
-            // 終了条件
-            if (target == NULL)
-            {
-                target = CreateUniform(uniform, target->prev, NULL);
-            }
-            else if (uniform < target->value)
-            {
-                struct Uniform *new = CreateUniform(uniform, target->prev, target);
-                target->prev->next = new;
-                target->prev = new;
-            }
-            else
-            {
-                AddUniform(target->next, uniform, 'r');
-            }
-            break;
-    }
-}
-
-void MoveUniform(struct Uniform *current, char result, char direction)
+void AddSample(struct Position *node, char result)
 {
     switch (result)
     {
         case 'w':
-            if (direction == 'r')
-            {
-                current = current->next;
-            }
+            result = 1;
             break;
-
+        
         case 'l':
-            if (direction == 'l')
-            {
-                current = current->next;
-            }
-            break;
-    }
-}
-
-void ClearRangeUniform(struct Uniform *node, int range, char direction)
-{
-    // 終了条件
-    if (node == NULL) return;
-    
-    switch (direction)
-    {
-        case 'l':
-            ClearRangeUniform(node->prev, range++, direction);
-            break;
-        case 'r':
-            ClearRangeUniform(node->next, range++, direction);
+            result = 0;
             break;
     }
 
-    if (range > UNIFORM_RANGE)
+    struct Sample *new;
+    if (node->sample == NULL)
     {
-        free(node);
-    }
-}
-
-// プレイアウト後に実行
-// 構造体が持つサンプルメンバを更新する
-struct Uniform* UpdateSample(struct Uniform *node, int result)
-{
-    float uniform = (float)rand() / RAND_MAX;
-
-    if (node == NULL)
-    {
-        return CreateUniform(uniform, NULL, NULL);
-    }
-
-    char direction;
-    if (uniform > node->value)
-    {
-        direction = 'r';
+        new = CreateSample(result, NULL);
     }
     else
     {
-        direction = 'l';
+        new = CreateSample(result, node->sample);
     }
-
-    AddUniform(node, uniform, direction); //ここどうなんだろ？nodeじゃないものを操作しているような…
-    MoveUniform(node, result, direction);
-    ClearRangeUniform(node, 0, direction);
+    node->sample = new;
+    node->sample_length++;        
 }
 
-void AccessUniform(struct Uniform *node)
+int CompareUniform(const void *a, const void *b)
 {
-    struct Uniform *current = node;
-    while (current->prev != NULL)
+    float compare = *(float*)a - *(float*)b;
+    if (compare > 0)
     {
-        current = current->prev;
+        return -1;
     }
-    while (current != NULL)
+    else
     {
-        printf("%f\n", current->value);
+        return 1;
+    }
+}
+
+float Sample(struct Position *node)
+{
+    int a = 1;
+    int b = 1;
+
+    int uniform_length = a + b;
+    if (node->sample_length < node->max_sample_length)
+    {
+        uniform_length += node->sample_length;
+    }
+    else
+    {
+        uniform_length += node->max_sample_length;
+    }
+    float uniforms[uniform_length];
+
+    struct Sample *current = node->sample;
+    uniforms[0] = (float)rand() / RAND_MAX;
+    uniforms[1] = (float)rand() / RAND_MAX;
+
+    for (int i = 2; i < uniform_length; i++)
+    {
+        uniforms[i] = (float)rand() / RAND_MAX;
+        if  (!current->result)
+        {
+            b++;
+        }
+        /*if (current->index == uniform_length)
+        {
+            current->next == NULL
+        }*/
         current = current->next;
     }
+
+    /*while(current != NULL)
+    {
+        // prevからのnext参照をクリアしないと
+        printf("%d\n", current->result);
+        struct Sample *tmp = current;
+        current = current->next;
+        free(tmp);
+        printf("free\n");
+    }*/
+
+    qsort(uniforms, uniform_length, sizeof(float), CompareUniform);
+
+    return uniforms[b]; // デフォルトでb=1なので-1しない
 }
 
-struct Position* CreatePosition(int a, int b)
+struct Position* CreatePosition(int max_sample_length)
 {
     struct Position *node = (struct Position *)malloc(sizeof(struct Position));
-    node->a = a;
-    node->b = b;
-    node->sample = NULL;
-    for (int i = 0; i < a; i++)
-    {
-        node->sample = UpdateSample(node->sample, 'w');
-    }
-    for (int i = 0; i < b; i++)
-    {
-        node->sample = UpdateSample(node->sample, 'l');
-    }
+
+    //struct Sample *tail = CreateSample(1, NULL);
+    //node->sample = CreateSample(0, tail);
+    node->sample_length = 0;
+    //AddSample(node, 'w');
+    //AddSample(node, 'l');
+    node->max_sample_length = max_sample_length;
 
     return node;
 }
 
-char Playout(float prob)
+void Playout(struct Position *node, float prob)
 {
     float uniform = (float)rand() / RAND_MAX;
+    char result;
     if (uniform < prob)
     {
-        return 'w';
+        result = 'w';
     }
     else
     {
-        return 'l';
+        result = 'l';
     }
+    AddSample(node, result);
 }
 
 int main(int argc, char *argv[])
 {
     printf("こんにちは\n");
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    int seed = atoi(argv[3]);
+    int seed = atoi(argv[1]);
     printf("Seedは%dです\n", seed);
     srand(seed);
 
-    struct Position *hoge = CreatePosition(a, b);
-    printf("リストを構成しました。\n");
+    struct Position *hoge = CreatePosition(5);
 
-    //AccessUniform(hoge->sample);
-
-    int trial = 1000;
-    float prob = 0.5;
+    int trial = 10;
+    float prob = 0.3;
     for (int i = 0; i < trial; i++)
     {
-        int result = Playout(prob);
-        hoge->sample = UpdateSample(hoge->sample, result);
-        printf("%f\n", hoge->sample->value);
+        Playout(hoge, prob);
+        float sample = Sample(hoge);
+        printf("%f\n", sample);
     }
-
-    /*
-    trial = 400;
-    prob = 0.3;
-    for (int i = 0; i < trial; i++)
-    {
-        int result = Playout(prob);
-        hoge->sample = UpdateSample(hoge->sample, result);
-        printf("%f\n", hoge->sample->value);
-    }
-    */
 
     return 0;
 }
