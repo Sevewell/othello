82478bf1011b799f608e72c91cf128e8f02dfb3c
yuta <tmon.seveny@gmail.com>
2020-10-01 22:39:02 +0900

    WebSocket接続リトライを実装

--- interface/index.js
+++ interface/index.js
@@ -1,55 +1,65 @@
-const ws = new WebSocket(`ws://${location.hostname}:8080/`);
+function connectWebSocket() {
 
-ws.onopen = function (event) {
-    ;
-};
+    let ws = new WebSocket(`ws://${location.hostname.replace('interface', 'engine')}:8080/`);
 
-ws.onmessage = function (event) {
+    ws.onopen = function (event) {
+        console.log('WebSocketに接続しました');
+    };
 
-    let status = JSON.parse(event.data);
-    console.log(status);
+    ws.onerror = function (event) {
 
-    if (status.field.seat) {
+        console.log('WebSocketに接続できませんでした');
+        console.log(event);
+        console.log('再接続を試みます');
+        ws.close();
+        setTimeout(connectWebSocket, 1000);
 
-        if (status.user.player) {
-            document.getElementById('seat').textContent = '席を離れる';
+    };
+
+    ws.onmessage = function (event) {
+
+        let status = JSON.parse(event.data);
+        console.log(status);
+
+        if (status.field.seat) {
+
+            if (status.user.player) {
+                document.getElementById('seat').textContent = '席を離れる';
+            } else {
+                document.getElementById('seat').textContent = '他の人が着席中';
+            }
+
         } else {
-            document.getElementById('seat').textContent = '他の人が着席中';
+
+            document.getElementById('seat').textContent = '席に座る';
+
         }
 
-    } else {
+        let black = status.field.black
+        let white = status.field.white
 
-        document.getElementById('seat').textContent = '席に座る';
+        for (let i = 0; i < 64; i++) {
+
+            let img = 'panel.png';
+            if (black[i] === '1' && white[i] === '1') {
+                console.log('Error: duplicate stone.');
+            }
+            if (black[i] === '1') {
+                img = 'black.png';
+            }
+            if (white[i] === '1') {
+                img = 'white.png'
+            }
+
+            let panel = document.getElementById(`panel_${i.toString().padStart(2, '0')}`);
+            panel.querySelector('img').setAttribute('src', img);
+
+        }
 
     }
 
-    let black = status.field.black
-    let white = status.field.white
-
-    for (let i = 0; i < 64; i++) {
-
-        let img = 'panel.png';
-        if (black[i] === '1' && white[i] === '1') {
-            console.log('Error: duplicate stone.');
-        }
-        if (black[i] === '1') {
-            img = 'black.png';
-        }
-        if (white[i] === '1') {
-            img = 'white.png'
-        }
-
-        let panel = document.getElementById(`panel_${i.toString().padStart(2, '0')}`);
-        panel.querySelector('img').setAttribute('src', img);
-
-    }
-
-    /*let message = document.getElementById('message');
-    let chat = document.createElement('p');
-    chat.textContent = data.info;
-    message.appendChild(chat);*/
-
-}
+    return ws;
+};
 
 function draw() {
 
@@ -89,3 +99,5 @@ function search() {
         key: 'search'
     }));
 }
+
+const ws = connectWebSocket();
