2e45a2ddb1d4cb4952a8b1e31bc955f4cf2da521
sevewell <sevewell@outlook.jp>
2026-03-20 22:16:26 +0900

    Rust版エンジンメインを実装

--- engine-rust/src/main.rs
+++ engine-rust/src/main.rs
@@ -1,5 +1,42 @@
 mod engine;
 
+use std::env;
+
+fn count_node(node: &engine::Node, count: &mut u32) {
+    *count += 1;
+    if !node.children.is_empty() {
+        for child in &node.children {
+            count_node(child, count);
+        }
+    }
+}
+
+fn print_node(node: engine::Node) {
+    let mut nodes: u32 = 0;
+    count_node(&node, &mut nodes);
+    print!("[");
+    for child in node.children {
+        print!("{{ ");
+        print!("\"move\": {}, ", (child.m | child.y) ^ (node.m | node.y));
+        print!("\"m\": {}, ", child.y);
+        print!("\"y\": {}, ", child.m);
+        print!("\"alpha\": {:.3}", child.alpha);
+        print!("\"nodes\": {}k", nodes / 1000);
+        print!(" }}, ");
+    }
+    print!("]");
+}
+
 fn main() {
-    println!("Hello World!");
+    let args: Vec<String> = env::args().collect();
+    let m: u64 = args[1].parse().unwrap();
+    let y: u64 = args[2].parse().unwrap();
+    let iter: u64 = args[3].parse().unwrap();
+    let mut node: engine::Node = engine::Node::new(m, y);
+    let mut result: engine::GameResult;
+    for i in 0..iter {
+        result = engine::GameResult::None;
+        engine::playout(&mut node, &mut result, false);
+    }
+    print_node(node);
 }
